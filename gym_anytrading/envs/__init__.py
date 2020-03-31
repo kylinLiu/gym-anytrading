2fb1e1b9233696c14a8120bf888df879e3958687
unknown <qinlin.liu@gticom.cn>
2020-03-31 15:01:28 +0800

    update env

--- gym_anytrading/envs/__init__.py
+++ gym_anytrading/envs/__init__.py
@@ -1,3 +1,4 @@
-from .trading_env import TradingEnv, Actions, Positions
+# from .trading_env import TradingEnv, Actions, Positions
+from .trading_env import TradingEnv, Actions
 from .forex_env import ForexEnv
 from .stocks_env import StocksEnv
