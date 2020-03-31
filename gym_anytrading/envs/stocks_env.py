e163e32108399f024ae02e6f0db3c27be0c31da2
Liumumu <247651305@qq.com>
2020-03-31 14:00:17 +0800

    Update stocks_env.py

--- gym_anytrading/envs/stocks_env.py
+++ gym_anytrading/envs/stocks_env.py
@@ -20,7 +20,8 @@ class StocksEnv(TradingEnv):
 
         prices[self.frame_bound[0] - self.window_size]  # validate index (TODO: Improve validation)
         prices = prices[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
-
+        print(typeof(prices))
+        raise Exception("xx")
         diff = np.insert(np.diff(prices), 0, 0)
         signal_features = np.column_stack((prices, diff))
 
