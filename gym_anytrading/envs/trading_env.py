d9649d410ebf6929162bde4f49a54037a13c19a4
unknown <qinlin.liu@gticom.cn>
2020-04-03 13:53:05 +0800

    current_tick

--- gym_anytrading/envs/trading_env.py
+++ gym_anytrading/envs/trading_env.py
@@ -52,6 +52,7 @@ class TradingEnv(gym.Env):
         # episode
         self._start_tick = self.window_size
         self._end_tick = len(self.prices) - 1
+        print("len self._end_tick",len(self._end_tick))
         self._done = None
         self._current_tick = None
         self._last_trade_tick = None
