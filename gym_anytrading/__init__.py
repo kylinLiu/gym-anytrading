c7d813a70042a780b63fcd736c7327bdfa8fe7a9
unknown <qinlin.liu@gticom.cn>
2020-04-24 10:29:57 +0800

    update

--- gym_anytrading/__init__.py
+++ gym_anytrading/__init__.py
@@ -22,3 +22,15 @@ register(
         'frame_bound': (30, len(datasets.STOCKS_GOOGL))
     }
 )
+
+def register_new(stock_code):
+    dataset = datasets.load_dataset_online('sz.000001')
+    register(
+        id='stocks-v0',
+        entry_point='gym_anytrading.envs:StocksEnv',
+        kwargs={
+            'df': deepcopy(dataset),
+            'window_size': 30,
+            'frame_bound': (30, len(dataset))
+        }
+    )
