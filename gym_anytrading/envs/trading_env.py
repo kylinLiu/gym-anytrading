4a6f86705da6aa2c458e709817a31297ee748b26
unknown <qinlin.liu@gticom.cn>
2020-05-24 11:41:37 +0800

    reshape

--- gym_anytrading/envs/trading_env.py
+++ gym_anytrading/envs/trading_env.py
@@ -178,7 +178,10 @@ class TradingEnv(gym.Env):
 
     def _get_observation(self):
         #         return self.signal_features[(self._current_tick-self.window_size):self._current_tick][:,0]
-        # return self.signal_features[(self._current_tick - self.window_size):self._current_tick][:, 1]
+        print((self.signal_features[(self._current_tick - self.window_size):self._current_tick][:, 1]).shape())
+        print((self.signal_features[(self._current_tick - self.window_size):self._current_tick]).shape())
+        print((self.signal_features[(self._current_tick - self.window_size):self._current_tick]).reshape(1,-1).shape())
+        print((self.signal_features[(self._current_tick - self.window_size):self._current_tick]).reshape(-1,1).shape())
         return self.signal_features[(self._current_tick - self.window_size):self._current_tick].reshape(1,-1)
 
     def render(self, mode='human'):
